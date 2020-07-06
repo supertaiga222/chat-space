5764780e85e956158caf9dd3846d409db336f48e
supertaiga222 <66627988+supertaiga222@users.noreply.github.com>
2020-07-06 21:01:45 +0900

    pull

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,45 @@
 $(function(){ 
-      function buildHTML(message){
-        if ( message.image ) {
-          var html =
-            `<div class="message">
-              <div class="upper-info">
-                <div class="upper-info__talker">
-                  ${message.user_name}
-                </div>
-                <div class="upper-info__date">
-                  ${message.created_at}
-                </div>
-              </div>
-              <div class="text">
-                <p class="text__content">
-                  ${message.content}
-                </p>
-              </div>
-              <img src=${message.image} >
-            </div>`
-          return html;
-        } else {
-          var html =
-            `<div class="message">
-              <div class="upper-info">
-                <div class="upper-info__talker">
-                  ${message.user_name}
-                </div>
-                <div class="upper-info__date">
-                  ${message.created_at}
-                </div>
-              </div>
-              <div class="text">
-                <p class="text__content">
-                  ${message.content}
-                </p>
-              </div>
-            </div>`
-          return html;
-        };
-    }
-$('#new_message').on('submit', function(e){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="text">
+            <p class="text__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="text">
+            <p class="text__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
     
     e.preventDefault();
     var formData = new FormData(this);
@@ -53,11 +53,9 @@ $('#new_message').on('submit', function(e){
       contentType: false
     })
       .done(function(data){
-        console.log("test")
-        console.log(data)
         var html = buildHTML(data);
         $('.messages').append(html);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
         $('form')[0].reset();
       })
       .always(() => {
@@ -66,5 +64,37 @@ $('#new_message').on('submit', function(e){
       .fail(function() {
         alert("メッセージ送信に失敗しました");
     });
-})
+  });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
